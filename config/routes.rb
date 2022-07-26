3c53884925c69b435527606221bad16e176bdc0f
Vinit-source <vinitgore1234@gmail.com>
2022-07-26 17:55:21 +0530

    Show formatted list of todos and access /todos/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file,see https://https://guides.rubyonrails.org/routing.html
+
+  # RESOURCE
+  # Todo is a resource
+  # - list of resources can be seen: index
+  # - single resource can be seen: show
+  # - create a new resource
+  # These are some standard emthods required for every resource. So rails provides resource routing (https://guides.rubyonrails.org/routing.html#resource-routing-the-rails-default)
+
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
