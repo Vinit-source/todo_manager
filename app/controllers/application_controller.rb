0f581aebbcb0a2131e21351b3fef351cd0ed9965
Vinit Gore <vinitgore1234@gmail.com>
2022-08-16 18:11:02 +0530

    Added index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def main
+        render "index"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   resources :todos
   post "/users/login", to: "users#login"
   resources :users
+  get "/", to: "application#main"
 end
